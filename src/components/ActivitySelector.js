7f3b4116b591bea463beeb32eec5dc7aad798f0f
Laura Dobie <moving.toyshop@gmail.com>
2021-01-04 15:02:29 +0000

    Slight change to select button

--- src/components/ActivitySelector.js
+++ src/components/ActivitySelector.js
@@ -9,7 +9,7 @@ function ActivitySelector({handleActivityTypeSelection}) {
         <div className="activity-selector">
             <h2>Need something</h2>
             <select defaultValue="" onChange={handleChange}>
-                <option value="" disabled>Activity type</option>
+                <option value="" disabled>random</option>
                 <option value="social">sociable</option>
                 <option value="relaxation">relaxing</option>
                 <option value="education">educational</option>                
@@ -17,7 +17,7 @@ function ActivitySelector({handleActivityTypeSelection}) {
                 <option value="cooking">edible</option>                
                 <option value="music">musical</option>                
                 <option value="busywork">industrious</option>                
-                <option value="free">free!</option>                
+                <option value="free">free</option>                
             </select>
             <h2>to fill the hours?</h2>
         </div>
